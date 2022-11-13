9267e9c136c1288c114f5eb78f9c06710e1bc862
valentineejk <valentineejk@gmail.com>
2022-11-13 18:19:00 +0100

    fix

--- components/Stack.js
+++ components/Stack.js
@@ -14,7 +14,7 @@ const stack = [
     { img: Flutter },
     { img: React1 },
     { img: Tw },
-    { img: Swift1 }, { img: Firebase1 },
+    { img: Firebase1 },
 
 
 ]
@@ -27,11 +27,11 @@ const Stack = () => {
             <div className=" grid  place-items-center h-56 " >
 
                 <h1 className="text-4xl font-bold m-6">Tech Stack</h1>
-                <div div className="avatar grid grid-cols-5 sm:grid-cols-5 gap-4 place-content-center h-48">
+                <div div className="avatar grid grid-cols-4 sm:grid-cols-4 gap-4 place-content-center h-48">
 
                     { stack.map((stacks) => (
 
-                        <div className="sm:w-24 w-12 shadow-sm rounded-[20px] shadow-purple-300 dark:shadow-purple-200 " key={ stacks.img }>
+                        <div className="sm:w-24 w-16 shadow-sm rounded-[20px] shadow-purple-300 dark:shadow-purple-200 " key={ stacks.img }>
                             <Image
                                 alt="valentine"
                                 src={ stacks.img }
