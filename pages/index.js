9baf152830258c6ea3c0ad49de9a7de14b0b7d41
valentineejk <valentineejk@gmail.com>
2022-11-13 19:22:55 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="Container">
+    <div >
       <Hero />
       <Stack />
       <Portfolio />
